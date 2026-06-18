73a9d5e3d30a25d7ad7afb6b35a07870832e71bb
numan00085-source <numan00085@gmail.com>
2026-06-18 19:09:25 -0400

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com', 'cdn.zelux.us'],
+  },
+}
+module.exports = nextConfig
