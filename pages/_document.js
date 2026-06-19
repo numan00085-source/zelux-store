23df8bb76eb38ad2a844b98cc215d9d2c71920cf
numan00085-source <numan00085@gmail.com>
2026-06-19 00:28:40 -0400

    Premium redesign: pages/_document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet" />
-        <meta name="theme-color" content="#0A0A0A" />
+        <meta name="theme-color" content="#060B16" />
       </Head>
       <body>
         <Main />
