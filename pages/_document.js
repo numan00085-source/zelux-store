b0668ae0009c1667bbb3cffa4e8e1e6d7f806fb2
numan00085-source <numan00085@gmail.com>
2026-06-20 17:30:50 -0400

    Add ZELUX favicon and app icons (Z mark, all standard sizes)

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,11 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet" />
         <meta name="theme-color" content="#060B16" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body>
         <Main />
