1e06220a3b5e5a60d379f1ea666b790e521e42be
numan00085-source <numan00085@gmail.com>
2026-06-23 22:18:41 -0400

    Add Google Search Console verification meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet" />
         <meta name="theme-color" content="#060B16" />
+        <meta name="google-site-verification" content="L1bdcgWjqWefyxB_CaZLPXJ_ycIQq9CfuvJoKKyS3j0" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
